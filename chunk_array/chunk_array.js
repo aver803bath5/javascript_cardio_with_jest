56179f6ba4cde0047eab16071e6cdc787e5fa597
g9mein <aver803r803bath261@gmail.com>
2019-09-11 16:10:05 +0800

    Fix chunk_array function and test

--- chunk_array/chunk_array.js
+++ chunk_array/chunk_array.js
@@ -1,16 +1,22 @@
 function chunkArray(arr, len) {
   let chunkedArr = [];
   let groupedArr = [];
+  let groupedArrElementCount = 0;
+  let nthEl = 0;
 
   arr.forEach((el, index) => {
-    let loopCount = index + 1;
     groupedArr.push(el);
+    groupedArrElementCount++;
+    nthEl = index + 1;
 
-    if (loopCount === len || loopCount = arr.length) {
+    if (groupedArrElementCount === len || nthEl === arr.length) {
       chunkedArr.push(groupedArr);
       groupedArr = [];
+      groupedArrElementCount = 0;
     }
   });
+
+  return chunkedArr;
 }
 
-module.exports(chunkArray);
+module.exports = chunkArray;
