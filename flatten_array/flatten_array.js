abbd1b144bda55b0890c10cc7b7cc8c5cbcb0c97
g9Mien <aver803bath261@gmail.com>
2019-09-13 20:38:25 +0800

    modify flatten_array function
    * use another method instead of prototype function to flat array
    * extract test argument and expected result form the test case

--- flatten_array/flatten_array.js
+++ flatten_array/flatten_array.js
@@ -1,5 +1,21 @@
+'use strict'
+
 function flattenArray(arr) {
-  return arr.flat();
+  const stack = [...arr];
+  const res = [];
+
+  while (stack.length) {
+    // Pop value from stack
+    const next = stack.pop();
+    if (Array.isArray(next)) {
+      // Push back items, won't modify the original input.
+      stack.push(next);
+    } else {
+      res.push(next);
+    }
+  }
+
+  return res;
 }
 
 module.exports = flattenArray;
--- flatten_array/flatten_array.test.js
+++ flatten_array/flatten_array.test.js
@@ -1,5 +1,8 @@
 const flattenArray = require('./flatten_array');
 
 test('take an array of arrays and flatten to a single array', () => {
-  expect(flattenArray([[1, 2], [3, 4], [5, 6], [7]])).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6, 7]));
+  const testArr = [[1, 2], [3, 4], [5, 6], [7]];
+  const expectedResult = [1, 2, 3, 4, 5, 6, 7];
+
+  expect(flattenArray(testArr)).toEqual(expect.arrayContaining(expectedResult));
 });
