879d2968a38aacb7558babec0eec48837e7eedaa
g9Mien <aver803bath261@gmail.com>
2019-09-15 19:58:36 +0800

    Add comment

--- flatten_array/flatten_array.js
+++ flatten_array/flatten_array.js
@@ -9,6 +9,7 @@ function flattenArray(arr) {
     const next = stack.pop();
     if (Array.isArray(next)) {
       // Push back items, won't modify the original input.
+      // Use seperator to extract one level of the array.
       stack.push(...next);
     } else {
       res.push(next);
